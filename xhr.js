73c4eb42c640a25523f5c7c2f2d6f947c28b7382
Richard Warren <rwarren92@hotmail.com>
2016-07-07 15:13:36 +0100

    Update xhr.js

--- xhr.js
+++ xhr.js
@@ -1,1 +1,10 @@
-
+function getData () {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState === 4 && xhr.status === 200) {
+            console.log('server responded with: ' + xhr.responseText);
+        }
+    }
+    xhr.open('get', '/' + input.value);
+    xhr.send();
+}
